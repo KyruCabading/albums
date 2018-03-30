3c69bb34e65f9eb7c7c2532e10038c142d14d44a
Kyru Cabading <kyrusri@gmail.com>
2018-03-30 22:01:42 +0800

    add Style & prop data

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,37 @@
 // Import Libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
-const Header = () => {
-  return <Text>Albums</Text>;
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>;
+    </View>
+  );
 };
+
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#111111',
+    shadowOffset: { width: 0, height: 1},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20,
+  }
+};
+
 
 // Make the component available to other parts of the app
 export default Header;
