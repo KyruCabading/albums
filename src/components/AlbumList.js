aefa44d6555cebb5b5e61e1ce3c791630fb42649
Kyru Cabading <kyrusri@gmail.com>
2018-03-30 23:14:14 +0800

    add fetch & re-render
    Fetch URL: https://rallycoding.herokuapp.com/api/music_albums
    To fetch data and have component re-render when fetch is complete.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  state = { albums: [] }
+
   componentWillMount() {
-    console.log('componentWillMount Now');
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(res => res.json())
+    .then(res => this.setState({ albums: res }));
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
-        <Text>Album List!!!!</Text>
+        <Text>Album List</Text>
       </View>
     );
   }
