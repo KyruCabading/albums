b7fa8e5cb151631a0c6f0e3af3bda9d1cb75d4c4
Kyru Cabading <kyrusri@gmail.com>
2018-03-31 05:14:19 +0800

    add AlbumDetailStyling
    Add Album Thumbnail image, Cover Image w/ Styling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,65 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    albumTitleStyle,
+    albumContentStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    albumCoverStyle
+  } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={albumContentStyle}>
+          <Text style={albumTitleStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={albumCoverStyle}
+          source={{ uri: image }} />
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  albumTitleStyle: {
+    fontSize: 16
+  },
+  albumContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  albumCoverStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
