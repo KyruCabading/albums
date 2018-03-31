74f4a9047abf7bc10d2358dc6dce13798edfcfc3
Kyru Cabading <kyrusri@gmail.com>
2018-03-31 15:23:30 +0800

    update LoginForm: add Email & Password

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  state = { email: '', password: '' };
+
   render() {
     return (
-      <View />
+      <Card>
+        <CardSection>
+          <Input
+            label="Email:"
+            placeholder="user@gmail.com"
+            value={this.state.email.toLowerCase()}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Password:"
+            placeholder="password"
+            secureTextEntry
+            value={this.state.password.toLowerCase()}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
+
+const styles = {
+  rowStyle: {
+    flexDirection: 'row'
+  },
+  rowTextStyle: {
+    width: 100,
+    paddingLeft: 20,
+    paddingRight: 20
+  }
+};
 
 export default LoginForm;
