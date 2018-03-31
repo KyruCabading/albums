ef6c835c3c63c52317938ae60a622e9bc2d937bf
Kyru Cabading <kyrusri@gmail.com>
2018-03-31 14:09:05 +0800

    add firebase auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 import AlbumList from './components/AlbumList';
 
+
 // Create a component
-const App = () => (
-  <View style={{ flex: 1 }}>
-    <Header headerText="Login"/>
-  </View>
-);
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyA6d-G7WhII4vbtpszVStJdOGpwdyc-dhg",
+      authDomain: "album-763e0.firebaseapp.com",
+      databaseURL: "https://album-763e0.firebaseio.com",
+      projectId: "album-763e0",
+      storageBucket: "album-763e0.appspot.com",
+      messagingSenderId: "475926031818"
+    });
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header headerText="Login" />
+        <Text>An App!</Text>
+      </View>
+    );
+  }
+}
+// const App = () => (
+//   <View style={{ flex: 1 }}>
+//     <Header headerText="Login"/>
+//   </View>
+// );
 
 // AlbumList
 // const App = () => (
